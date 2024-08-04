c54ba6e2d39d081bbbecd3c727aad99e8e539d8a
Ahmed Emara <ahmedhossamemara8@gmail.com>
2024-08-04 21:46:43 +0300

    added navbar and home page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import ResponsiveAppBar from './components/header/Navbar';
 import Home from './components/home/home';
 
 function App() {
   return (
     <div className="App">
+      <ResponsiveAppBar />
       <Home />
     </div>
   );
--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,8 +1,29 @@
-import ResponsiveAppBar from '../header/Navbar';
+import React from "react";
+import './home.css';
 
-function Home(){
-    return(
-        <ResponsiveAppBar />
+function Home() {
+    return (
+        <div className="container">
+            <div className="description">
+                <div className="description-text">
+                    <h1 style={{ color: 'white' }}>
+                        I'm <span style={{ color: 'aquamarine' }}>Ahmed Emara</span>.
+                        Fresh Graduate in Computer Science from AAST | Former Intern at ITWorx, Cairo, Egypt.
+                    </h1>
+                </div>
+                <div className="img-container">
+                    <img src="./assets/myPhoto.png" alt="Ahmed Emara"></img>
+                </div>
+            </div>
+            <div className="btns">
+                <a href='/#about' className='about-link'>
+                    More about me
+                </a>
+                <button className="projects-btn">
+                    View my projects
+                </button>
+            </div>
+        </div>
     );
 }
 export default Home;
